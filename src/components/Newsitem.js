b85cf5f60d6965b8cdd4725046ed432bf3930d1e
MukeshRwt <rawatms003@gmail.com>
2024-05-19 00:41:50 +0530

    first commit

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,24 +1,50 @@
-import { render } from '@testing-library/react';
-import React  from 'react'
+import React from "react";
 
-const Newsitem =(props)=> {
-    let {Title, description, imageUrl, newsUrl,author,date,source}= props;
-    return (
-      <div className='my-3'>
-            <div className="card" >
-              <div style={{display:'flex', justifyContent:'flex-end', position:'absolute', right:'0'}}>
-            <span className=" badge rounded-pill bg-danger" >{source} </span>
-            </div>
-            <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2023/10/17/1600x900/CRICKET-ICC-MENS-WC-2023-RSA-NED-ODI-52_1697550437661_1697550456804.jpg":imageUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-            <h5 className="card-title">{Title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text"><small className="text-body-secondary">By {!author?"Unknown": author} on {new Date(date).toGMTString()}</small></p>
-            <a rel='noreferrer' href={newsUrl} target='_blank' className="btn btn-sm btn-primary btn-dark">Read More</a>
-            </div>
-            </div>
+const Newsitem = (props) => {
+  let { Title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <img
+          src={
+            !imageUrl
+              ? "https://www.hindustantimes.com/ht-img/img/2023/10/17/1600x900/CRICKET-ICC-MENS-WC-2023-RSA-NED-ODI-52_1697550437661_1697550456804.jpg"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{Title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-primary btn-dark"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
     </div>
-    )
-  }
+  );
+};
 
-export default Newsitem
+export default Newsitem;
